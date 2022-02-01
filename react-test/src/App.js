a073d56c2f32504dc8898d77f598f7b5ea304cd2
DooZeeR <zsolt.ordas@gmail.com>
2022-02-01 15:12:33 +0100

    posts

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,9 +1,43 @@
+import { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  function add() {
+    setPosts([...posts,{
+      title: "Random",
+      postText: "Szöveg4",
+      date: "2022.02.04"
+    }])
+  }
+
   return (
     <div className="App">
+      <h1>Blog Posts</h1>
 
+      <div>
+        <input type="text"  placeholder='title'/>
+        <input type="text"  placeholder='post'/>
+        <button onClick={add}>New Post</button>
+        <button onClick={() => {setPosts([])}}>Delete all posts</button>
+      </div>
+
+      {posts.map((item) => {
+        return (
+          <div key={item.title}>
+            <h2>{item.title}</h2>
+            <p>{item.postText}</p>
+            <p>{item.date}</p>
+            <input type="text" placeholder='New post title' />
+            <input type="text" placeholder='New post text' />
+            <button>Update</button>
+            <button>Delete</button>
+          </div>
+        )
+      })}
     </div>
   );
 }
